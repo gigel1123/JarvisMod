b5559fe7688206c5d0f8c2430f9534a1f91f817a
Gigel123 <andrei045632@gmail.com>
2026-06-22 18:27:24 +0300

    stremio plugin

--- plugins/open_stremio_app.py
+++ plugins/open_stremio_app.py
@@ -1,26 +1,20 @@
-import os
-
 # plugins/test_plugin.py
-
+import os
+from src import talk
 # 1. The action name Jarvis and Llama 3 will use in the JSON payload
-PLUGIN_NAME = "open_stremio"
+PLUGIN_NAME = "open_stremio_app"
 
 # 2. The instruction injected into the AI's system prompt telling it when to use this
-PLUGIN_DESC = '"test_print" (no arguments. Use this function when the user explicitly asks to run a test or check if plugins are working)'
+PLUGIN_DESC = '"open_stremio_app" (no arguments. Use this function when asked to open stremio which is an app but you will use specifically open stremio for this app and not open app!)'
 
 
 # 3. The actual function Jarvis executes when the action matches
 def run():
-    import talk
-
+    os.startfile("stremio.exe")
     # Print to your console terminal
     print("\n[PLUGIN SUCCESS] The dynamic drop-in test plugin was called successfully!")
-    os.startfile("stremio.exe")
-
-    # Bonus: You can also open a specific file or URL directly in its default app!
-    os.startfile("C:\\Users\\YourName\\Documents\\report.docx")
 
     # Make Jarvis speak to confirm it works
-    talk.jarvis("Opening Stremio now sir!")
+    talk.jarvis("Yes sir! Opening stremio NOW!")
 
     return "test plugin executed successfully"
